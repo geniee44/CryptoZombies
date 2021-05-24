d3198d81ef517ae17338fdc24f1e67d702c861b2
geniee44 <68368633+geniee44@users.noreply.github.com>
2021-05-24 16:59:31 +0900

    Update zombiefeeding.sol

--- Lesson3/zombiefeeding.sol
+++ Lesson3/zombiefeeding.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
@@ -30,7 +30,7 @@ contract ZombieFeeding is ZombieFactory {
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+    if (keccak256(_species) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
